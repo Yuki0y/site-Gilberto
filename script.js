5758c3686c5922b5b2f37b657134fd2cb4a1fa34
Yuki <yukiphb@gmail.com>
2025-07-01 16:21:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Exibe alerta ao enviar formulário
+document.getElementById("formContato").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Mensagem enviada com sucesso!");
+});
